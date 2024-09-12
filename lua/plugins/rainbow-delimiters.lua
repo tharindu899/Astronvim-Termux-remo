607cf3c0d5923bf86cb846a8c29ccd19ad04c5a6
@Remo773 <ramamajhi14@gmail.com>
2024-09-12 22:35:21 +0530

    loading issue fixed rainbow-delimiters

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,39 +1,41 @@
 -- ~/.config/nvim/lua/plugins/rainbow-delimiters.lua
 return {
   {
-    "hiphish/rainbow-delimiters.nvim",
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "BufRead",  -- Loads the plugin when a buffer is read
     config = function()
-      local rainbow_delimiters = require('rainbow-delimiters')
-      -- Define the custom colors
-      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkRed", { fg = "#ff2c2c", bold = true })      -- Dark Red
-      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkYellow", { fg = "#FFE761", bold = true })   -- Dark Yellow
-      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkBlue", { fg = "#2a9df4", bold = true })     -- Dark Blue
-      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkOrange", { fg = "#FFA652", bold = true })   -- Dark Orange
-      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkGreen", { fg = "#39f077", bold = true })    -- Dark Green
-      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkViolet", { fg = "#924dbf", bold = true })   -- Dark Violet
-      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkCyan", { fg = "#33c7d8", bold = true })     -- Dark Cyan
+      -- Import the rainbow-delimiters library
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      -- Define custom colors for rainbow delimiters
+      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkRed", { fg = "#ff2c2c", bold = true })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkYellow", { fg = "#FFE761", bold = true })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkBlue", { fg = "#2a9df4", bold = true })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkOrange", { fg = "#FFA652", bold = true })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkGreen", { fg = "#39f077", bold = true })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkViolet", { fg = "#924dbf", bold = true })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterDarkCyan", { fg = "#33c7d8", bold = true })
 
       -- Configure rainbow delimiters
       vim.g.rainbow_delimiters = {
         strategy = {
-          [''] = rainbow_delimiters.strategy['global'],
-          vim = rainbow_delimiters.strategy['local'],
+          [''] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["local"],
         },
         query = {
-          [''] = 'rainbow-delimiters',
-          lua = 'rainbow-blocks',
+          [''] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
         },
         highlight = {
-          'RainbowDelimiterDarkRed',
-          'RainbowDelimiterDarkYellow',
-          'RainbowDelimiterDarkBlue',
-          'RainbowDelimiterDarkOrange',
-          'RainbowDelimiterDarkGreen',
-          'RainbowDelimiterDarkViolet',
-          'RainbowDelimiterDarkCyan',
+          "RainbowDelimiterDarkRed",
+          "RainbowDelimiterDarkYellow",
+          "RainbowDelimiterDarkBlue",
+          "RainbowDelimiterDarkOrange",
+          "RainbowDelimiterDarkGreen",
+          "RainbowDelimiterDarkViolet",
+          "RainbowDelimiterDarkCyan",
         },
       }
     end,
-  },
+  }
 }
-
