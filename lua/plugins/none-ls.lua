5521d25cc6e1e5e6b2df35f550abe38468b2b0d4
@Remo773 <ramamajhi14@gmail.com>
2024-10-29 07:40:57 +0530

    none-ls update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,4 +1,5 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- Remove the warning line to activate this file
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize None-ls sources
 
@@ -7,7 +8,6 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
     -- opts variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -16,9 +16,15 @@ return {
     -- Only insert new sources, do not replace the existing ones
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      -- Add clang-format as a formatter
+      require("null-ls").builtins.formatting.clang_format.with({
+        filetypes = { "c", "cpp", "objc", "objcpp" }, -- Specify the filetypes for clang-format
+      }),
+      -- Add stylua as a formatter
+      require("null-ls").builtins.formatting.stylua.with({
+        filetypes = { "lua" }, -- Specify the filetypes for stylua
+        command = "/data/data/com.termux/files/usr/bin/stylua", -- specify the path to stylua if necessary
+      }),
     })
   end,
 }
